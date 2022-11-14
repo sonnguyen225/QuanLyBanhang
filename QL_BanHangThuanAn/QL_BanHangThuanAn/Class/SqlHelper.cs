39fab44072a7f787788715753ed4b4a82c1848de
Thanhson987 <maysonca@gmail.com>
2022-11-14 20:20:14 +0700

    Push code đăng nhập/ đăng xuất/ tìm kiếm thông tin sản phẩm

---
+++ QL_BanHangThuanAn/QL_BanHangThuanAn/Class/SqlHelper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace QL_BanHangThuanAn
+{
+    class SqlHelper
+    {
+        public static int loainguoidung;
+        public static string tennguoidung;
+        public static string ConnectString = @"Data Source=NTAN\SQLEXPRESS;AttachDbFilename=" + Application.StartupPath + @"\Quanlybanhang.mdf;Integrated Security=True;Connect Timeout=30;User Instance=True";
+        public static SqlConnection con;
+    }
+}
